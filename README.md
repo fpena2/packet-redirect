22ba685e6b3c3551371ca6aa0155c624860685af
fpena2 <f.j.pena@wustl.edu>
2024-04-27 23:50:01 -0500

    Pass ES PDU data to clients

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Setup
+
+```cmd
+sudo apt install python3-venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install git+https://github.com/open-dis/open-dis-python
+deactivate
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use dis_rs::entity_state::model::EntityState;
 use dis_rs::enumerations::PduType;
-use dis_rs::model::PduBody;
+use dis_rs::model::{Location, Orientation, PduBody, VectorF32};
 use futures::SinkExt;
+use nalgebra::Vector3;
+use serde::{Deserialize, Serialize};
+use std::f64::consts::PI;
 use tokio::net::UdpSocket;
 use tokio::sync::watch; // mpsc
 use tokio::task::JoinSet; // for websocket.send()
+
+// This will be sent out as a dictionary of EntityStructures.
+// { EntityId : EntityStructure }
+#[derive(Debug, Serialize, Deserialize)]
+struct EntityStructure {
+    marking: String,
+    position: [f64; 3],
+    angle: f32,
+    id: u16,
+}
 
 #[tokio::main]
 async fn main() {
@@ -59,13 +72,52 @@ async fn handle_client_connection(
 
         // Send data to client
         let pdu = rx.borrow_and_update().clone();
-        let received_string = pdu.entity_marking.marking_string;
+        let packet = create_entity_structure_packet(pdu);
         ws_stream
-            .send(tokio_tungstenite::tungstenite::Message::Text(
-                received_string,
-            ))
+            .send(tokio_tungstenite::tungstenite::Message::Text(packet))
             .await?;
     }
+}
+
+fn create_entity_structure_packet(pdu: EntityState) -> String {
+    let packet = EntityStructure {
+        marking: pdu.entity_marking.marking_string.clone(),
+        position: [
+            pdu.entity_location.x_coordinate,
+            pdu.entity_location.y_coordinate,
+            pdu.entity_location.z_coordinate,
+        ],
+        angle: calculate_angle(
+            pdu.entity_location,
+            pdu.entity_orientation,
+            pdu.entity_linear_velocity,
+        ),
+        id: pdu.entity_id.entity_id,
+    };
+    serde_json::to_string(&packet).expect("Serialization failed")
+}
+
+fn calculate_angle(_location: Location, orientation: Orientation, _velocity: VectorF32) -> f32 {
+    // let loc = Vector3::new(
+    //     location.x_coordinate,
+    //     location.y_coordinate,
+    //     location.z_coordinate,
+    // );
+    // let euler = Vector3::new(orientation.phi, orientation.phi, orientation.phi);
+    // let vel = Vector3::new(
+    //     velocity.first_vector_component as f64,
+    //     velocity.second_vector_component as f64,
+    //     velocity.third_vector_component as f64,
+    // );
+    //
+    // let linear_velocity = vel.cross(&loc);
+    // let linear_velocity_magnitude = linear_velocity.magnitude();
+    //
+    // // Calculate angle in xy-plane
+    // let theta = linear_velocity.y.atan2(linear_velocity.x);
+    // // Calculate angle with respect to z-axis
+    // let phi = linear_velocity.z.acos() / PI;
+    orientation.psi
 }
 
 fn print_new_connection(address: &std::net::SocketAddr) {
