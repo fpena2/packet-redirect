32b74981269afec9633c2cc16c88daccbf18d888
fpena2 <f.j.pena@wustl.edu>
2024-11-13 11:01:30 -0600

    add test

--- src/producers.rs
+++ src/producers.rs
@@ -21,3 +21,35 @@ impl ProducerHandler {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::sync::watch;
+
+    #[tokio::test]
+    async fn test_producer_handler() {
+        let (tx, mut rx) = watch::channel(String::new());
+
+        // Init ProducerHandler
+        let handler = ProducerHandler::new(tx).await;
+        let monitor_task = tokio::spawn(async move {
+            handler.monitor().await.unwrap();
+        });
+
+        // Send a test message
+        let test_socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let test_message = "Hello, World!";
+        test_socket
+            .send_to(test_message.as_bytes(), "127.0.0.1:3000")
+            .await
+            .unwrap();
+
+        // Check if the message was distributed to the channel receivers
+        rx.changed().await.unwrap();
+        let received_message: String = rx.borrow_and_update().clone();
+        assert_eq!(received_message, test_message);
+
+        monitor_task.abort();
+    }
+}
